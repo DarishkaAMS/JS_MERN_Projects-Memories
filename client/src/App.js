68df734d506b36f9ba1ef836db905fecd9c99433
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-30 14:17:56 +0300

    Post & Form @ components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import memories from './images/memories.png';
 
 const App = () => {
     return (
-        <div>
-            <h2>Hello DarishkaAMS!</h2>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center"> My Memories </Typography>
+                <img src={memories} alt="memories" height="60"/>
+            </AppBar>
+        </Container>
     );
-}
+};
 
 export default App;

9be223a794b503224e8d4a499c3dbb5ce4f481af
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-30 16:01:00 +0300

    Add form and posts

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post/Post.js'
+
+const Posts = () => {
+    return (
+        <>
+        <h2>POSTS</h2>
+        <Post />
+        <Post />
+        </>
+    );
+}
+
+export default Posts;

c182f8e8d546b54d8b39a53f1269038fc0220d77
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-01 07:25:43 +0300

    Add getPosts controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,19 @@
-export const getPosts = (req, res) => {
-    res.send('It is Alive!!!');
+import PostMessage from '../models/postsMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    };
+};
+
+export const createPosts = (req, res) => {
+    try {
+
+    } catch (error) {
+
+    };
 };

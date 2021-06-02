c4bc823bdf714d6f689733346300600f687b15ee
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-02 07:29:08 +0300

    Add redux

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Post from './Post/Post.js';
 import useStyles from './styles';
 
 const Posts = () => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+    console.log(posts);
+
     return (
         <>
-        <h2> Posts to come </h2>
-        <Post />
-        <Post />
-        <Post />
+            <h2> Posts to come </h2>
+            <Post />
+            <Post />
+            <Post />
         </>
     )
 };

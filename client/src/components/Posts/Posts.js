50631f51c35558a7a3d8e82b1fa658b2e983be03
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-02 06:36:08 +0300

    Add styles @ from, post and posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Post from './Post/Post.js';
-
+import useStyles from './styles';
 
 const Posts = () => {
+    const classes = useStyles();
     return (
         <>
         <h2> Posts to come </h2>

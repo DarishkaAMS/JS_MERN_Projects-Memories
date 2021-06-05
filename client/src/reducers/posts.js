65ab1650195ba903aa943369943c485bbb136c7b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-05 12:42:03 +0300

    Update delete logic - FE

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,16 +3,16 @@ import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionType
 export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
-      return action.payload;
-    case LIKE:
-      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+        return action.payload;
     case CREATE:
-      return [...posts, action.payload];
+        return [...posts, action.payload];
     case UPDATE:
-      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+        return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
     case DELETE:
-      return posts.filter((post) => post._id !== action.payload);
+        return posts.filter((post) => post._id !== action.payload);
+    case LIKE:
+        return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
     default:
-      return posts;
+        return posts;
   }
 };

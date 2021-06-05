3adc513c354ff60880f2b4f139504f6126e122c4
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-05 14:20:35 +0300

    Prepare for deployment

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 // const CONNECTION_URL = 'CONNECTION_URL';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))

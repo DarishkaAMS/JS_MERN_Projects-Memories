9511574d0eda01600dd69f83aedf8e40508a1d2c
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-05 12:57:04 +0300

    Add like logic

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
   
 import express from 'express';
 
-import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import { getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
 const router = express.Router();
 

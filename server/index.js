ef0174a5f5863fd2f9ff02351ad560da70992a99
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-13 06:54:12 +0300

    Update post

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 app.get('/', postRoutes);
-// app.get('/', (req, res) => {
+// app.get('/', (req, res) => {`
 //   res.send('Hello to DarishkaAMS Memories API');
 // });
 

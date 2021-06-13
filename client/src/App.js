e6350baea0600ac194bf5da40ded375c895fd46f
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-13 12:43:24 +0300

    Add Auth component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRoute, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Auth from './components/Auth/Auth';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
 
 const App = () => (
+  <BrowserRouter>
     <Container maxWidth="lg">
       <Navbar />
-      <Home />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
     </Container>
+  </BrowserRouter>
 );
 
 export default App;

a43a35b61cf7e7af09431880b2944f009062a944
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-13 12:33:00 +0300

    Add Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRoute, Route, Switch } from 'react-router-dom';
 
-import Form from './components/Form/Form';
-import Posts from './components/Posts/Posts';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
-import { getPosts } from './actions/posts';
-import useStyles from './styles';
-import memories from './images/memories.png';
 
-const App = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-  const classes = useStyles();
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
-  return (
+const App = () => (
     <Container maxWidth="lg">
       <Navbar />
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Home />
     </Container>
-  );
-};
+);
 
 export default App;

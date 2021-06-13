349f9cdc742d79f6900b4bb2b9daa78e7fd57943
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-13 11:55:42 +0300

    Remove Navbar to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
-import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import Navbar from './components/Navbar/Navbar';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
 import memories from './images/memories.png';
@@ -19,10 +20,7 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
-      </AppBar>
+      <Navbar />
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>

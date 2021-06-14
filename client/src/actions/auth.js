83a44a954b463f6c5edb16b4658d62d0c22398e3
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-14 06:41:17 +0300

    Add auth actions

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,9 +3,9 @@ import * as api from '../api/index.js';
 
 export const signin = (formData, history) => async (dispatch) => {
     try {
-        // const { data } = await api.signIn(formData);
+        const { data } = await api.signIn(formData);
 
-        // dispatch({ type: AUTH, data });
+        dispatch({ type: AUTH, data });
 
         history.push('/');
     } catch (error) {
@@ -15,9 +15,9 @@ export const signin = (formData, history) => async (dispatch) => {
 
 export const signup = (formData,history) => async (dispatch) => {
     try {
-        // const { data } = await api.signUp(formData);
+        const { data } = await api.signUp(formData);
 
-        // dispatch({ type: AUTH, data });
+        dispatch({ type: AUTH, data });
 
         history.push('/');
     } catch (error) {
